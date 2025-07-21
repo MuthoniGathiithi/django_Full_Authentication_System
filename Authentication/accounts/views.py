42aead7c0381d9f2daabf4a48b3c8c61f5253d9a
Joyce <gathiithijoyce74@gmail.com>
2025-07-21 14:29:49 +0300

    created login_view in accounts app views.py

--- Authentication/accounts/views.py
+++ Authentication/accounts/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from django.contrib.auth import authenticate, login
+
 def signup_view(request):
     if request.method == 'POST':
-        form=SignUpForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            user=form.save(commit=False)
+            user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
             return redirect('login')
-        else:
-            form=SignUpForm()
-            return render(request,'signup.html',{'form':form})
-        
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')        
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    return render(request, 'login.html')
+
