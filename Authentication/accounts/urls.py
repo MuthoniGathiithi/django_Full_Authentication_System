3630f995fcd59ce2ff822bc99e4965fea7d91149
Joyce <gathiithijoyce74@gmail.com>
2025-07-21 14:56:59 +0300

    created in apps level account  urls.py

---
+++ Authentication/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+    path('', views.login_view, name='home'),  # Root URL redirects to login
+]
