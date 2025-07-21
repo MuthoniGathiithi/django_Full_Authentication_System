87f4b68df39118010064327cfd740e256e5f3521
Joyce <gathiithijoyce74@gmail.com>
2025-07-21 14:20:59 +0300

    created signup_view in views.py of the accounts app

--- Authentication/accounts/views.py
+++ Authentication/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import SignUpForm
+def signup_view(request):
+    if request.method == 'POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+        else:
+            form=SignUpForm()
+            return render(request,'signup.html',{'form':form})
+        
 
 # Create your views here.
